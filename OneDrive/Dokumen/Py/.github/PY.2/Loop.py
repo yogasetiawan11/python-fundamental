101fb2c13da5a9c739ac2584c6bc10624a0df75c
yoga setiawan <setiawan4business@gmail.com>
2025-05-02 14:59:12 +0700

    adding aditional py

---
+++ OneDrive/Dokumen/Py/.github/PY.2/Loop.py
@@ -0,0 +1,14 @@
+'''Tujuan: Mengulang sesuatu dengan jumlah yang pasti.
+Kita biasanya pakai for ketika:
+
+Sudah tahu berapa kali harus mengulang.
+
+Mau mengulang setiap item di dalam list, string, atau range angka.'''
+
+animal = ['cat', 'dog', 'fish', 'fly']
+for animals in animal:
+    print(animals)
+
+#print i yang ada di dalam list animal
+# tambah : di akhir 
+# tambah kan spasi di print
