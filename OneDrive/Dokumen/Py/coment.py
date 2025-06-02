3e812eed2cefe5b2478fcaff08604352bf822715
yoga.setiawan11 <setiawan4business@gmail.com>
2025-06-02 20:40:09 +0700

    coment.py

--- OneDrive/Dokumen/Py/coment.py
+++ OneDrive/Dokumen/Py/coment.py
@@ -1,11 +1,7 @@
 #learn py 17 april 2025
 
-'''Triple quotes juga digunakan untuk memberikan penjelasan 
- fungsi, kelas, atau modul. Ini disebut docstring.
- Dengan docstring, kode Python menjadi lebih mudah dipahami,
- baik untuk programmer lain yang membaca kode'''
+''' '''         <-- # you can use these both to add comment more than 1 line
+"""  """        <--
+# # # #         <-- # you can use this comment to add only 1 line
 
- """sama seperti di atas. Fungsi ini menyapa seseorang.
-    Argumen:
-    nama -- nama orang yang ingin disapa """
 
